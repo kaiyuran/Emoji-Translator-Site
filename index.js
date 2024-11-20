5a2a442fe275b944feb7128f85e73c2cff339904
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2024-11-20 17:04:34 -0500

    cleanup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function convertEmoji() {
     let ogMessageArr = ogMessage.split("\n");
     console.log(ogMessageArr);
     // console.log(ogMessage);
-    // let url = "http://127.0.0.1:5000/getemoji?message=";
+    // let url = "http://127.0.0.1:33303/getemoji?message=";
     let url = "http://37.27.51.34:33303/getemoji?message="
     
 
@@ -19,12 +19,11 @@ function convertEmoji() {
         promises.push(
             fetch(url + ogMessageArr[i])
                 .then((response) => response.json())
-                .then((json) => ({ index: i, message: json['message'] })) // Include the index with the response
+                .then((json) => ({ index: i, message: json['message'] }))
         );
     }
     
     Promise.all(promises).then((results) => {
-        // Sort the results by the original order using the index
         results.sort((a, b) => a.index - b.index);
     
         for (let result of results) {
